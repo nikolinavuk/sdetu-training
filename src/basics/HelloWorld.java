4d2c5577a679257bb346a5abda351be7dcf6c218
Nikolina Vukelic <G01373@oneadr.net>
2020-01-01 13:14:13 +0100

    Modified the helloWorld class

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,6 @@ package basics;
 public class HelloWorld {
 
     public static void main (String args []){
-        System.out.println("Hello world");
+        System.out.println("Hello world and 2020");
     }
 }
